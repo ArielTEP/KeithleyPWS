ba55b06f3d20a380565ce762f3a16b319a5fe4fe
Ariel Tonatiuh <espindola_ipn@hotmail.com>
2017-09-18 18:40:23 -0500

    Update: Now saving in a file the resulted measures

--- PWS/PWS.cpp
+++ PWS/PWS.cpp
@@ -3,50 +3,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-ViSession defaultRM; //Resource manager id
-ViSession PWS4000; //Identifies the power supply
-long ErrorStatus;
-char commandString[256];
-char ReadBuffer[256];
-
-void OpenPort();
-void SendSCPI(char* pString);
-void CheckError(char* pMessage);
-void delay(clock_t wait);
-double readVoltage();
-double readCurrent();
-void ClosePort();
+#define RES "USB::0x05E6::0x2200::1358231::INSTR"
+FILE *file;
 
 
 void main(int argc, _TCHAR* argv[]) 
 {
 	printf("Iniciando...\n");
 	double voltage;
-	char Buffer[256];
 	double current;
-	OpenPort();
-	//Query the power supply id, read response and print it
-	sprintf(Buffer, "*IDN?");
-	SendSCPI(Buffer);
-	printf("Instrument identification string:%s \n", Buffer);
-	SendSCPI("*RST"); //reset the power supply
-	SendSCPI("OUTPUT 1"); // turn output on
-
-	voltage = 3.0;
-	current = 0.1;
-	printf("Setting voltage(V) & current(A): %f,%f \n",	voltage, current);
-	ErrorStatus = viPrintf(PWS4000, "VOLT %f\n", voltage); //set the output voltage
-	CheckError("Unable to set voltage");
-	ErrorStatus = viPrintf(PWS4000, "CURRENT %f\n", current);
-	//set the output current
-	CheckError("Unable to set current");
-
-	// begin measurement 
-	delay(100);
-	voltage = readVoltage();
-	current = readCurrent();
-	printf("Measured voltage(V) & current(A): %f,%f \n", voltage, current);
 	
+	// turn output on
+	OpenPort();
+	SendSCPI("OUTPUT 1"); 
+	
+	// set output voltage and current
+	setMeasure(3.0f, 0.1f);
+
+	// begin measurement
+	const int samples = 20;
+	double voltageArray[samples];
+	double currentArray[samples];
+	int i;
+	for (i = 0; i < samples; i++) 
+	{
+		delay(100);
+		voltageArray[i] = readVoltage();
+		currentArray[i] = readCurrent();
+	}
+	
+	writeFile(voltageArray, currentArray, samples);
+
+	// end of work
 	getch();
 	SendSCPI("OUTPUT 0"); //turn output off
 	SendSCPI("SYSTEM:LOCAL");
@@ -55,14 +43,56 @@ void main(int argc, _TCHAR* argv[])
 	getch();
 }
 
+void setMeasure(double voltage, double current) 
+{
+	printf("Setting voltage(V) & current(A): %f,%f \n", voltage, current);
+	ErrorStatus = viPrintf(PWS4000, "VOLT %f\n", voltage);
+	CheckError("Unable to set voltage");
+	ErrorStatus = viPrintf(PWS4000, "CURRENT %f\n", current);
+	CheckError("Unable to set current");
+}
+
+void writeFile(double* voltageArray, double* currentArray, const int samples)
+{
+	char string[512];
+	char fullpath[64];
+	printf("\nNombre del archivo (especifique el formato): ");
+	scanf("%s", string);
+	fflush(stdin);
+	strcpy(fullpath, "C:\\PWSDatabase\\");
+	strcat(fullpath, string);
+	printf("Creado en: %s\n", fullpath);
+	printf("\nPresione intro para continuar...\n");
+	getch();
+
+	file = fopen(fullpath, "w");
+	int i;
+	for (i = 0; i < samples; i++) { // escritura del archivo
+		fprintf(file, "%lf,%lf\n", voltageArray[i], currentArray[i]);
+		printf("Measured voltage(V) & current(A): %f,%f \n", voltageArray[i], currentArray[i]);
+	}
+	printf("Listo.\n");
+	fclose(file);
+}
+
+void identifyResource() 
+{
+	char Buffer[256];
+	OpenPort();
+	sprintf(Buffer, "*IDN?");
+	SendSCPI(Buffer);
+	printf("Instrument identification string:%s \n", Buffer);
+	SendSCPI("*RST"); //reset the power supply
+	ClosePort();
+}
+
 void OpenPort()
 {
 	//Open communication session with the power supply, and
 	// put the power supply in remote
 	ErrorStatus = viOpenDefaultRM(&defaultRM);
-	ErrorStatus = viOpen(defaultRM,
-		"USB::0x05E6::0x2200::1358231::INSTR", 0, 0, &PWS4000);
-	CheckError("Unable to open the port");
+	ErrorStatus = viOpen(defaultRM, RES, 0, 0, &PWS4000);
+	CheckError("La fuente esta desconectada. Conectela y reinicie el programa.");
 	SendSCPI("SYSTEM:REMOTE");
 }
 
@@ -72,14 +102,14 @@ void SendSCPI(char* pString)
 	strcpy(commandString, pString);
 	strcat(commandString, "\n");
 	ErrorStatus = viPrintf(PWS4000, commandString);
-	CheckError("Can't Write to Power Supply");
+	CheckError("No se puede escribir la Fuente");
 	pdest = strchr(commandString, '?'); //Search for query command
 	if (pdest != NULL)
 	{
 		ErrorStatus = viBufRead(PWS4000,
 			(ViBuf)ReadBuffer,
 			sizeof(ReadBuffer), VI_NULL);
-		CheckError("Can't read from driver");
+		CheckError("No se pudo leer nada del Driver");
 		strcpy(pString, ReadBuffer);
 	}
 }
@@ -126,4 +156,26 @@ void delay(clock_t wait)
 	clock_t goal;
 	goal = wait + clock();
 	while (goal > clock());
+}
+
+void displayHelp()
+{
+	printf("************************************\n");
+	printf("*            A Y U D A             *\n");
+	printf("************************************\n");
+	printf("\n\nVerifique que la fuente este encendida y correctamente\n");
+	printf("conectada a la PC por medio del cable USB.\n");
+	getch();
+}
+
+void displayInfo()
+{
+	printf("************************************\n");
+	printf("*       A C E R C A  D E	   *\n");
+	printf("************************************\n");
+	printf("\n\nEste software es de uso libre, escrito especificamente para\n");
+	printf("la fuente programable KEITHLEY 2200-32-3\n");
+	printf("Fecha de creacion: 30/05/2017 en SEPI-Telecom ESIME\n");
+	printf("Autor: Alumno de Servicio Social\n");
+	getch();
 }
--- PWS/pws.h
+++ PWS/pws.h
@@ -4,10 +4,19 @@
 #include <conio.h>
 #include <string.h>
 
+ViSession defaultRM; //Resource manager id
+ViSession PWS4000; //Identifies the power supply
+
+long ErrorStatus;
+char commandString[256];
+char ReadBuffer[256];
+
 void OpenPort();
 void SendSCPI(char* pString);
 void CheckError(char* pMessage);
 void delay(clock_t wait);
 double readVoltage();
 double readCurrent();
+void setMeasure(double voltage, double current);
+void writeFile(double* voltageArray, double* currentArray, const int samples);
 void ClosePort();
